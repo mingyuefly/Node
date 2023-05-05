5298b7cd6282c6c3cef1c0ff41898170f9e157c3
gmy <>
2023-05-05 15:15:57 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('hello world')
+
+console.log(Date)
+console.log(Math)
+
+console.log(setTimeout)
+console.log(setInterval)
+
+console.log(__filename)
+console.log(__dirname)
+
+console.log(process)
