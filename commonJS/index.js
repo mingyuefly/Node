fa66e8a2166642f8eb0d4efaf54829f544cd37f2
gmy <>
2023-05-05 18:05:59 +0800

    commonJS game

---
+++ commonJS/index.js
@@ -0,0 +1,34 @@
+
+var playerAction = process.argv[process.argv.length - 1]
+
+const game = require('./lib.js')
+
+// const result = game(playerAction)
+// console.log(result)
+
+let count = 0
+process.stdin.on('data', e=> {
+    const playerAction = e.toString().trim()
+
+    const result = game(playerAction)
+    if (result == -1) {
+        count++
+    }
+    if (count === 3) {
+        console.log('你太厉害了，我不玩了')
+        process.exit()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+// console.log('start require')
+// var lib = crequire('./lib.js')
+// console.log('end require', lib)
