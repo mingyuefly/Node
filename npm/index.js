73dcfddad0ee67c1bf8dffea1c795255a6e85560
gmy <>
2023-05-07 18:09:05 +0800

    Node.js 事件循环

--- npm/index.js
+++ npm/index.js
@@ -1,25 +1,105 @@
+/**
+ * Node.js 异步编程
+ */
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(()=>{
+        resolve()
+    }, 500)
+})
+
+console.log(promise)
+
+setTimeout(()=>{
+    console.log(promise)
+}, 800)
+
+
+
+
+
+/**
+ * Node.js 事件循环
+ */
+
+// const eventloop = {
+//     queue:[],
+//     loop() {
+//         while (this.queue.length) {
+//             var callback = this.queue.shift()
+//             callback()
+//         }
+//         setTimeout(this.loop.bind(this), 50)
+//     },
+//     add(callback) {
+//         this.queue.push(callback)
+//     }
+// }
+
+// eventloop.loop();
+
+// setTimeout(() => {
+//     eventloop.add(function() {
+//         console.log(1)
+//     })
+// }, 500)
+
+// setTimeout(() => {
+//     eventloop.add(function() {
+//         console.log(2)
+//     })
+// }, 800)
+
+
+
+// try {
+//     interview(function() {
+//         console.log('smile')
+//     })
+// } catch (e) {
+//     console.log('cry', e);
+// }
+
+// interview(function(error, res) {
+//     if (error) {
+//         return console.log('cry')
+//     }
+//     console.log('smile')
+// })
+
+// function interview(callback) {
+//     setTimeout(() => {
+//         if (Math.random() < 0.1) {
+//             callback(null, 'success')
+//         } else {
+//             callback(new Error('fail')) 
+//         }
+//     }, 500);
+// }
+
+
 
 
 /**
  * 非阻塞I/O
  */
 
-const glob = require('glob');
+// const glob = require('glob');
 
-console.time('sync')
-const result = glob.sync(__dirname + '/**/*')
-console.timeEnd('sync')
-console.log(result.length)
+// console.time('sync')
+// const result = glob.sync(__dirname + '/**/*')
+// console.timeEnd('sync')
+// console.log(result.length)
 
 
 
-console.time('async')
-const result2 = glob(__dirname + '/**/*', function (err, result) {
-    console.log(result.length)
-})
-console.timeEnd('async')
-// IO完成之前还可以做别的事
-console.log('hello geekbang')
+// console.time('async')
+// const result2 = glob(__dirname + '/**/*', function (err, result) {
+//     console.log(result.length)
+// })
+// console.timeEnd('async')
+// // IO完成之前还可以做别的事
+// console.log('hello geekbang')
 
 
 
