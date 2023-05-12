95678bbf3f477eec816037ef534ff2eed9dcc3bc
gmy <>
2023-05-12 20:33:14 +0800

    commit

--- geektime/ES6Template/index.js
+++ geektime/ES6Template/index.js
@@ -1,46 +1,46 @@
-// const ejs = require('ejs')
+const ejs = require('ejs')
 
-// let bgColor='red';
-// let divColor = `当前div背景颜色是${ bgColor }`;
-// console.log(divColor); //当前div背景颜色是red
+let bgColor='red';
+let divColor = `当前div背景颜色是${ bgColor }`;
+console.log(divColor); //当前div背景颜色是red
 
-// let a=1;
-// let b=2;
-// let c=`${a+b}`
-// console.log(c)
+let a=1;
+let b=2;
+let c=`${a+b}`
+console.log(c)
 
-// const user = {
-//     // name:'haha'
-//     name:'<script />'
-// }
+const user = {
+    // name:'haha'
+    name:'<script />'
+}
 
-// console.log(`${user.name}`)
-// const userName = `user的名字是${user.name}`
-// console.log(userName)
+console.log(`${user.name}`)
+const userName = `user的名字是${user.name}`
+console.log(userName)
 
-// const result = `<h2>${user.name}</h2>`
-// console.log(result)
-
-// const vm = require('vm')
-// console.log(vm.runInNewContext('`<h2>${user.name}</h2>`', {user}))
-// //过滤script
-// console.log(vm.runInNewContext('`<h2>${_(user.name)}</h2>`', {
-//     user,
-//      _: function(markup) {
-//         if (!markup) return ''
-//         return String(markup)
-//             .replace(/&/g, '&amp;')
-//             .replace(/</g, '&lt;')
-//             .replace(/>/g, '&gt;')
-//             .replace(/'/g, '&#39;')
-//             .replace(/"/g, '&quot;')
-//     }
-// }))
-
-
-
+const result = `<h2>${user.name}</h2>`
+console.log(result)
 
 const vm = require('vm')
+console.log(vm.runInNewContext('`<h2>${user.name}</h2>`', {user}))
+//过滤script
+console.log(vm.runInNewContext('`<h2>${_(user.name)}</h2>`', {
+    user,
+     _: function(markup) {
+        if (!markup) return ''
+        return String(markup)
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/'/g, '&#39;')
+            .replace(/"/g, '&quot;')
+    }
+}))
+
+
+
+
+// const vm = require('vm')
 
 //template 
 const templateMap = {
